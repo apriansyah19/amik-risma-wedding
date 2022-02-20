c749030dc8af16de4e8683f3b2b4018ce2f890e5
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-02-21 01:58:59 +0700

    fix(content): fixing css not look good

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -122,7 +122,7 @@
       class="min-h-screen w-screen flex bg-[#fff] font-akaya"
       id="mempelai-wanita"
     >
-      <div class="min-h-1/2 w-full m-auto pb-28">
+      <div class="min-h-1/2 w-full m-auto pb-32">
         <img src="../../public/decoration-2.png" class="w-full mb-4" />
         <h1
           class="text-center text-3xl mb-4 font-honey"
@@ -179,7 +179,7 @@
       class="min-h-screen w-screen flex bg-[#fff] font-akaya"
       id="mempelai-pria"
     >
-      <div class="min-h-1/2 w-full m-auto">
+      <div class="min-h-1/2 w-full m-auto pb-32">
         <img src="../../public/decoration-2.png" class="w-full mb-4" />
         <h1
           class="text-center text-3xl mb-8 font-honey"
