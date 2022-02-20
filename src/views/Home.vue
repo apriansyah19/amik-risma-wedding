6edcadb66ac7a3970f1d594e8ea61ef0cd926884
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-02-21 01:29:23 +0700

    fix(content): adding photos on mempelai section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -122,16 +122,32 @@
       class="min-h-screen w-screen flex bg-[#fff] font-akaya"
       id="mempelai-wanita"
     >
-      <div class="min-h-1/2 w-full m-auto">
+      <div class="min-h-1/2 w-full m-auto pb-16">
         <img src="../../public/decoration-2.png" class="w-full mb-4" />
         <h1
-          class="text-center text-3xl mb-8 font-honey"
+          class="text-center text-3xl mb-4 font-honey"
           data-aos="zoom-in-down"
           data-aos-duration="1500"
           data-aos-once="true"
         >
           Mempelai Wanita
         </h1>
+
+        <div class="w-full relative">
+          <img src="../assets/images/ta-1.jpg" class="mb-2" alt="" />
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+            class="absolute bottom-0"
+          >
+            <path
+              fill="#fff"
+              fill-opacity="1"
+              d="M0,160L40,165.3C80,171,160,181,240,208C320,235,400,277,480,277.3C560,277,640,235,720,224C800,213,880,235,960,245.3C1040,256,1120,256,1200,245.3C1280,235,1360,213,1400,202.7L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+            ></path>
+          </svg>
+        </div>
 
         <h2
           class="text-center text-4xl mb-8 font-honey"
@@ -173,6 +189,22 @@
         >
           Mempelai Pria
         </h1>
+
+        <div class="w-full relative">
+          <img src="../assets/images/ta-2.jpg" class="mb-2" alt="" />
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+            class="absolute bottom-0"
+          >
+            <path
+              fill="#fff"
+              fill-opacity="1"
+              d="M0,160L40,165.3C80,171,160,181,240,208C320,235,400,277,480,277.3C560,277,640,235,720,224C800,213,880,235,960,245.3C1040,256,1120,256,1200,245.3C1280,235,1360,213,1400,202.7L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+            ></path>
+          </svg>
+        </div>
 
         <h2
           class="text-center text-4xl mb-8 font-honey"
@@ -389,23 +421,23 @@ export default {
   created() {
     AOS.init();
   },
-  watch: {
-    modal() {
-      if (this.modal === false) {
-        var i = 0;
-        setInterval(() => {
-          if (i > 4) {
-            i = 0;
-            document.getElementById(this.list[i]).click();
-            i++;
-          } else {
-            document.getElementById(this.list[i]).click();
-            i++;
-          }
-        }, 5000);
-      }
-    },
-  },
+  // watch: {
+  //   modal() {
+  //     if (this.modal === false) {
+  //       var i = 0;
+  //       setInterval(() => {
+  //         if (i > 4) {
+  //           i = 0;
+  //           document.getElementById(this.list[i]).click();
+  //           i++;
+  //         } else {
+  //           document.getElementById(this.list[i]).click();
+  //           i++;
+  //         }
+  //       }, 5000);
+  //     }
+  //   },
+  // },
 };
 </script>
 
